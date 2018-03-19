e5a37c6d057ab6fc1fd731298cbd08d5b4ce67f1
Vincent Lee <vlee@vlee.me.uk>
2018-03-19 18:57:58 +0000

    Intial code upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Random-String-Generator
-Creates a random string of letter to the length you need
+# Random string generator
+This is a tool for generating random length string of your designer length.
+
+![Image showing window](/Images/header.png)
+
+### Requirements
+
+You'll need to install python 3.6 (*Or newer*) available from https://www.python.org/
+
+## Usage
+- **Install python if you haven't yet**
+
+
+1. Download the `main.py` file
+2. Double click on `main.py` to run it
+3. enter the length of the string you need
+4. Paste the string to anywhere you need it. *(The result is copied for you automatically)*
+
+#### Notes
+This is a quick programme I made for generating test data for my controlled assessment. I will not be offering assistance to use this tool as it was ment for personal use.
---
+++ main.py
@@ -0,0 +1,36 @@
+# Import packages
+import random  # Used for picking random letters
+import subprocess  # Used to interact with OS to run copy function
+import time  # Used to sleep shell to keep window open
+
+
+# Function to copy string to user's clipboard
+def copy2clip(txt):
+    cmd = 'echo '+txt.strip()+'|clip'
+    return subprocess.check_call(cmd, shell=True)
+
+
+# Specifies what letters to be used
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# Takes user input
+needed = input("Please enter the length of string required: ")
+
+# Initialises Variable
+needed = int(needed)-1
+start = (random.choice(letters))
+string = start
+
+# Generates and appends remaining needed characters
+for x in range(0, needed):
+    selected = (random.choice(letters))
+    string = string+selected
+
+# Print out final string
+print('===============================================================================================================')
+print('Final String is:')
+print(string)
+copy2clip(string)  # Copies string to user's clipboard
+print('This has been copied to your clipboard!')
+print('===============================================================================================================')
+time.sleep(5)
